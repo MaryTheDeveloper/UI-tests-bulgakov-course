421bcdef5b86bdd3ac57224eb058889e3b05311e
Мария Сатарова <marythedeveloper@gmail.com>
2026-06-19 15:52:45 +0300

    refactored test, create DemoQARegistrationPage PO class

---
+++ src/test/java/ru/bulgakov/pages/DemoQaRegistrationPage.java
@@ -0,0 +1,71 @@
+package ru.bulgakov.pages;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DemoQaRegistrationPage {
+
+    public final SelenideElement registrationForm = $("#userForm");
+    public final SelenideElement submitButton = $("#submit");
+    public final SelenideElement calendarForm = $(".react-datepicker-popper");
+    public final SelenideElement calendarMonthSelector = $(".react-datepicker__month-select");
+    public final SelenideElement calendarYearSelector = $(".react-datepicker__year-select");
+    public final ElementsCollection calendarMonthSelectorElements = $$(".react-datepicker__month-select option");
+    public final ElementsCollection calendarYearSelectorElements = $$(".react-datepicker__year-select option");
+    public final ElementsCollection calendarDaySelectorElements = $$(".react-datepicker__day");
+
+    public final SelenideElement registrationSuccessModal = $(".modal-content");
+    public final SelenideElement registrationInformationTable = $(".table-dark");
+    public final SelenideElement studentNameField = $(".table-dark > tbody > tr:first-child > td:nth-child(2)");
+
+    public DemoQaRegistrationPage registrationFormIsVisible() {
+        registrationForm.shouldBe(visible);
+        return this;
+    }
+
+    public DemoQaRegistrationPage setFieldById(String fieldName, String fieldValue) {
+        $("#" + fieldName).setValue(fieldValue);
+        return this;
+    }
+
+    public DemoQaRegistrationPage setRadioButtonByValue(String value) {
+        $(String.format("[value=%s]", value)).click();
+        return this;
+    }
+
+    public DemoQaRegistrationPage clickSubmitButton() {
+        submitButton.scrollTo().click();
+        return this;
+    }
+
+    public DemoQaRegistrationPage clickDateOfBirthField() {
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker-popper").shouldBe(visible);
+        return this;
+    }
+
+    public DemoQaRegistrationPage chooseDateOnTheTable(String day, String month, String year) {
+        calendarMonthSelector.click();
+        calendarMonthSelectorElements.findBy(text(month)).click();
+        calendarYearSelector.click();
+        calendarYearSelectorElements.findBy(text(year)).click();
+        calendarDaySelectorElements.findBy(text(day)).click();
+        calendarForm.shouldNotBe(visible);
+        return this;
+    }
+
+    public DemoQaRegistrationPage checkRegistrationSuccessModalTableVisible() {
+        registrationSuccessModal.shouldBe(visible);
+        registrationInformationTable.shouldBe(visible);
+        return this;
+    }
+
+    public DemoQaRegistrationPage checkFieldsSavedCorrecly(String firstName, String lastName) {
+        studentNameField.shouldHave(exactText(firstName + " " + lastName));
+        return this;
+    }
+}
--- src/test/java/ru/bulgakov/qa/QaTest.java
+++ src/test/java/ru/bulgakov/qa/QaTest.java
@@ -1,15 +1,13 @@
 package ru.bulgakov.qa;
 
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import ru.bulgakov.pages.DemoQaRegistrationPage;
 import ru.bulgakov.pages.LavaTopPayingPage;
 import ru.bulgakov.pages.YandexSearchPage;
-import ru.bulgakov.pages.YandexSearchResultsPage;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class QaTest {
@@ -39,19 +37,16 @@ public class QaTest {
         String lastName = "Doe";
         String phoneNumber = "8800555353";
 
-        open("https://demoqa.com/automation-practice-form");
-        sleep(3000);
-        $("#userForm").shouldBe(visible);
-
-        $("#firstName").setValue(firstName);
-        $("#lastName").setValue(lastName);
-        $("[value=\"Male\"]").click();
-        $("[placeholder=\"Mobile Number\"]").setValue(phoneNumber);
-        $("#submit").scrollTo().click();
-        sleep(3000);
-
-        $(".modal-content").shouldBe(visible);
-        $(".table-dark").shouldBe(visible);
-        $(".table-dark > tbody > tr:first-child > td:nth-child(2)").shouldHave(exactText(firstName + " " + lastName));
+        open("https://demoqa.com/automation-practice-form", DemoQaRegistrationPage.class)
+                .registrationFormIsVisible()
+                .setFieldById("firstName", firstName)
+                .setFieldById("lastName", lastName)
+                .setFieldById("userNumber", phoneNumber)
+                .setRadioButtonByValue("Male")
+                .clickDateOfBirthField()
+                .chooseDateOnTheTable("12","February", "1980")
+                .clickSubmitButton()
+                .checkRegistrationSuccessModalTableVisible()
+                .checkFieldsSavedCorrecly(firstName, lastName);
     }
 }
