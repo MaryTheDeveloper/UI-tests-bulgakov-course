e71dea2d9a21bd4b46295c5e1c4374c713f825b2
Мария Сатарова <marythedeveloper@gmail.com>
2026-06-28 14:12:31 +0300

    fix first test and flaky methods

--- src/test/java/ru/bulgakov/qa/QaTest.java
+++ src/test/java/ru/bulgakov/qa/QaTest.java
@@ -11,22 +11,24 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class QaTest {
 
+    private static final String PRACTICE_FORM_URL = "https://demoqa.com/automation-practice-form";
+    private static final String YANDEX_URL = "https://ya.ru/";
+
     @Test
     @DisplayName("Проверить, что цена обучения 47000.00 рублей")
     @Tag("POSITIVE")
     void mentoringPriceShouldBe47000Test() {
         LavaTopPayingPage lavaTopPayingPage = new LavaTopPayingPage();
 
-        open("https://ya.ru/", YandexSearchPage.class)
+        open(YANDEX_URL, YandexSearchPage.class)
                 .search("bulgakov qa")
                 .submit()
                 .closeDefaultBrowserSelectionPage()
                 .openLink("ivanbulgakovqa.ru")
                 .clickPrice()
-                .clickButton("Хочу вкатиться в QA")
-                .clickButton("Бегу оплачивать");
-
-        lavaTopPayingPage.checkPrice("47 000.00");
+                .wannaBeQA()
+                .readyToPay()
+                .checkPrice("47 000.00");
     }
 
     @Test
@@ -36,7 +38,7 @@ public class QaTest {
         String lastName = "Doe";
         String phoneNumber = "8800555353";
 
-        open("https://demoqa.com/automation-practice-form", DemoQaRegistrationPage.class)
+        open(PRACTICE_FORM_URL, DemoQaRegistrationPage.class)
                 .registrationFormIsVisible()
                 .setFirstName(firstName)
                 .setLastName(lastName)
